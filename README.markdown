29aaa5b9bff9dac873bb8ae5d3e615edb5fb2531
Brandon Aaron <brandon.aaron@gmail.com>
2008-10-15 21:21:59 -0500

    adding overlabel plugin

---
+++ README.markdown
@@ -0,0 +1,10 @@
+# overlabel
+
+A jQuery plugin to help implement the over label technique described in [Making Compact Forms More Accessible by Mike Brittain on ALA](http://alistapart.com/articles/makingcompactformsmoreaccessible).
+
+
+## License
+
+The batch plugin is dual licensed *(just like jQuery)* under the [MIT](http://www.opensource.org/licenses/mit-license.php) and [GPL](http://www.opensource.org/licenses/gpl-license.php) licenses.
+
+Copyright (c) 2008 [Brandon Aaron](http://brandonaaron.net)
---
+++ jquery.overlabel.js
@@ -0,0 +1,27 @@
+/*! Copyright (c) 2008 Brandon Aaron (http://brandonaaron.net)
+ * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) 
+ * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
+ *
+ * Based on Making Compact Forms More Accessible by Mike Brittain (http://alistapart.com/articles/makingcompactformsmoreaccessible)
+ */
+
+(function($){
+
+$.fn.overlabel = function() {
+	this.each(function() {
+		var $label = $(this), $input = $('#' + $label.attr('for'));
+
+		$label
+			.addClass('overlabel')
+			.bind('click', function(event) {
+				$input.focus();
+			});
+
+		$input
+			.bind('focus blur', function(event) {
+				$label[ (event.type == 'blur' && !$input.val() ? 'show' : 'hide') ]();
+			}).trigger('blur');
+	});
+};
+
+})(jQuery);

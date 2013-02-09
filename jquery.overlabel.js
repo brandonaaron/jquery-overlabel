0757dbf493157e9efc7787fd5b47021f26ddb809
Brandon Aaron <brandon.aaron@gmail.com>
2013-02-09 14:44:44 -0600

    support amd loaders and bump version

--- jquery.overlabel.js
+++ jquery.overlabel.js
@@ -1,29 +1,35 @@
-/*! Copyright (c) 2010 Brandon Aaron (http://brandonaaron.net)
+/*! Copyright (c) 2013 Brandon Aaron (http://brandonaaron.net)
  * Licensed under the MIT License (LICENSE.txt).
  *
- * Version 1.0
+ * Version 1.1.0
  *
  * Based on Making Compact Forms More Accessible by Mike Brittain (http://alistapart.com/articles/makingcompactformsmoreaccessible)
  */
 
-(function($){
+ (function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+    $.fn.overlabel = function() {
+        this.each(function() {
+            var $label = $(this),
+                $input = $('#' + $label.attr('for'));
 
-$.fn.overlabel = function() {
-    this.each(function() {
-        var $label = $(this),
-            $input = $('#' + $label.attr('for'));
+            $label
+                .addClass('overlabel')
+                .bind('click', function(event) {
+                    $input.focus();
+                });
 
-        $label
-            .addClass('overlabel')
-            .bind('click', function(event) {
-                $input.focus();
-            });
-
-        $input
-            .bind('focus blur', function(event) {
-                $label.css('display', (event.type == 'blur' && !$input.val() ? '' : 'none'));
-            }).trigger('blur');
-    });
-};
-
-})(jQuery);
+            $input
+                .bind('focus blur', function(event) {
+                    $label.css('display', (event.type == 'blur' && !$input.val() ? '' : 'none'));
+                }).trigger('blur');
+        });
+    };
+}));
